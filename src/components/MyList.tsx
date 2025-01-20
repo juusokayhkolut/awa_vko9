e2ef980b8dde12ec0983a07baf1bac54fdc682be
Juuso Kayhko <juusookayhko@gmail.com>
2025-01-20 21:39:16 +0200

    New commit

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -23,14 +23,14 @@ const MyList: React.FC<ListProps> = ({ header, items, toggleItemClick }) => {
             <li
               key={item.id}
               onClick={() => toggleItemClick && toggleItemClick(item.id)}
-              style={{ textDecoration: item.clicked ? "line-through" : "none" }}
+              style={{ textDecoration: item.clicked ? "line-through" : "" }}
             >
               {item.text}
             </li>
           ))
         ) : (
           <li
-          style={{ textDecoration: "none" }}
+          style={{ textDecoration: "" }}
           >No items available</li>
         )}
       </ol>
